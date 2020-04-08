3b3f89fd0420557035ddc9f9fa781e3a321a1036
Stas Fedorko <stasfedorko@MacBook-Pro-Stas.local>
2020-04-09 00:14:15 +0300

    Updates

---
+++ precache-manifest.95863d38644b09a5ce7a98952ad87e25.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1edbcff4f028bfaa8da8dd3fcb182842",
+    "url": "/hw16r/index.html"
+  },
+  {
+    "revision": "ca935ab32a8ea9862b33",
+    "url": "/hw16r/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "03272a0674a7c3844fd7",
+    "url": "/hw16r/static/css/main.c13640bf.chunk.css"
+  },
+  {
+    "revision": "ca935ab32a8ea9862b33",
+    "url": "/hw16r/static/js/2.466f9c7d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/hw16r/static/js/2.466f9c7d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "03272a0674a7c3844fd7",
+    "url": "/hw16r/static/js/main.f6d300a5.chunk.js"
+  },
+  {
+    "revision": "29a050836f5b32b82e4c",
+    "url": "/hw16r/static/js/runtime-main.98beaf1e.js"
+  },
+  {
+    "revision": "959502927ccb67c273e2790ce980c90a",
+    "url": "/hw16r/static/media/female.95950292.png"
+  },
+  {
+    "revision": "1dea51bcaf8f4a1fc24f7256da67a65c",
+    "url": "/hw16r/static/media/male.1dea51bc.png"
+  }
+]);
